af1830a8ca8848ec3cc0d84798f6f1221ad7b924
megha.su <megha.su@PIA3298>
2022-12-01 17:02:05 +0530

    Add project files.

---
+++ Interface[Square,Circle,Rectangle]/InterfaceClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface_Square_Circle_Rectangle_
+{
+   
+        interface IGeometry
+        {
+            void FindArea();
+
+            void FindPerimeter();
+
+        }
+
+        class Circle : IGeometry
+        {
+            double pi = 3.147;
+            public float radius;
+            public void Radius()
+            {
+
+                Console.WriteLine("Enter The Radius of Circle : ");
+                radius = float.Parse(Console.ReadLine());
+
+            }
+            public void FindArea()
+            {
+                double circlearea = pi * radius * radius;
+                Console.WriteLine("The Area Of Circle : " + circlearea);
+            }
+
+            public void FindPerimeter()
+            {
+                double circleperimeter = 2 * pi * radius;
+                Console.WriteLine("The Perimeter Of Circle : " + circleperimeter);
+            }
+
+        }
+
+        class Rectangle : IGeometry
+        {
+            public float rectanglebredth;
+            public float rectanglelength;
+
+            public void RectangleParameters()
+            {
+
+                Console.WriteLine("Enter The Length of Rectangle : ");
+                rectanglelength = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter The Bredth of Rectangle : ");
+                rectanglebredth = float.Parse(Console.ReadLine());
+
+            }
+            public void FindArea()
+            {
+                float rectanglearea = rectanglelength * rectanglebredth;
+                Console.WriteLine("The Area Of Rectangle : " + rectanglearea);
+            }
+
+            public void FindPerimeter()
+            {
+                float rectangleperimeter = 2 * (rectanglelength + rectanglebredth);
+                Console.WriteLine("The Perimeter Of Rectangle : " + rectangleperimeter);
+            }
+        }
+
+        class Square : IGeometry
+        {
+            public float squareside;
+            public void Squareside()
+            {
+
+                Console.WriteLine("Enter The Length of Square : ");
+                squareside = float.Parse(Console.ReadLine());
+
+            }
+
+            public void FindArea()
+            {
+                float squarearea = squareside * squareside;
+                Console.WriteLine("The  Area Of Square : " + squarearea);
+            }
+
+            public void FindPerimeter()
+            {
+                float squareperimeter = 4 * squareside;
+                Console.WriteLine("The Perimeter Of Square : " + squareperimeter);
+
+            }
+
+        }
+}
---
+++ Interface[Square,Circle,Rectangle]/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace Interface_Square_Circle_Rectangle_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your choice 1.Circle 2.Square 3.Rectangle");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+
+            case 1:
+
+            Circle circle = new Circle();
+            circle.Radius();
+
+            Console.WriteLine("AREA OF CIRCLE");
+            circle.FindArea();
+
+
+            Console.WriteLine("PERIMETER OF CIRCLE");
+            circle.FindPerimeter();
+            
+            break;
+
+            case 2:
+
+            Square square = new Square();
+            square.Squareside();
+
+            Console.WriteLine("AREA OF SQUARE");
+            square.FindArea();
+
+
+            Console.WriteLine("PERIMETER OF SQUARE");
+            square.FindPerimeter();
+
+            break;
+
+            case 3:
+
+             Rectangle rectangle = new Rectangle();
+             rectangle.RectangleParameters();
+
+             Console.WriteLine("AREA OF RECTANGLE");
+             rectangle.FindArea();
+
+             Console.WriteLine("PERIMETER OF RECTANGLE");
+             rectangle.FindPerimeter();
+
+            break;
+
+            default:
+
+            Console.WriteLine("Invalid choice");
+            break;
+
+            }
+        }    
+    }
+}
+
+
+
+
